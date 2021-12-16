ac2cfd8dd05b3b771a82acb00f21853001bc0bef
Sanket Patil <patilsanket209@gmail.com>
2021-12-16 15:34:11 +0530

    added data to local stroage

---
+++ dairy.js
@@ -0,0 +1,15 @@
+dairyData=[
+    {
+        imgUrl:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/394/original/data?1625864720",
+        name:"ISKCON's A2 Cream Ghee",
+        qty:"225ml",
+        price:"Rs.375"
+    },{
+        imgUrl:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/367/original/data?1622452513",
+        name:"ISKCON's A2 Bilano Ghee",
+        qty:"500ml",
+        price:"Rs.1250"
+    }
+
+]
+ localStorage.setItem("dairyData",JSON.stringify(dairyData))
