9d0d25dc5e9b6d524954d7c745e8a6b9f42f4e69
Venkat  R <venkygowdru99@gmail.com>
2021-12-17 00:16:59 +0530

    new update

---
+++ goto.js
@@ -0,0 +1,15 @@
+function gotoabt() {
+  window.location.href = "";
+}
+function gotoreg() {
+  window.location.href = "signup.html";
+}
+function gotocart() {
+  window.location.href = "";
+}
+function gotologin() {
+  window.location.href = "signin.html";
+}
+function gotoindex() {
+  window.location.href = "index.html";
+}
