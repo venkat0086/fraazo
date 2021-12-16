fb3f60bc44fb8983bb6ce04e950e8b20491bab2e
Venkat  R <venkygowdru99@gmail.com>
2021-12-16 17:29:49 +0530

    Update signin.js

--- signin.js
+++ signin.js
@@ -12,7 +12,7 @@ function signIn(event) {
     }
   }
   if (count > 0) {
-    window.location.href = "prod.html";
+    window.location.href = "index.html";
     window.alert("You Login Successful");
   } else {
     window.alert("Invalid Login Credentials");
