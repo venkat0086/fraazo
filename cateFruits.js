fe6e4514dc7edbc4cae44b8a1d49983659e78860
Sanket Patil <patilsanket209@gmail.com>
2021-12-16 11:51:30 +0530

    added data to local stroage
    set data to local stroage

---
+++ cateFruits.js
@@ -0,0 +1,46 @@
+fruitData=[
+    {
+        imgUrl:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN14.png",
+        name:"Banana Robusta",
+        qty:"1 kg",
+        price:"₹ 29"
+
+    },{
+        imgUrl:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN24.png",
+        name:"Banana Elaichi/ Yellaki",
+        qty:"1 kg",
+        price:"₹ 39"
+    },{
+        imgUrl:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FCHI13.png",
+        name:"Chikoo",
+        qty:"1 kg",
+        price:"₹ 49"
+    },{
+        imgUrl:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FGRA42.png",
+        name:"Grapes Green",
+        qty:"1 kg",
+        price:"₹ 70"  
+    },{
+        imgUrl:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/233/original/data?1609860341",
+        name:"Grapes Black",
+        qty:"1 kg",
+        price:"₹ 90"  
+    },{
+        imgUrl:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/369/original/data?1622631944",
+        name:"Guava White Indian",
+        qty:"1 kg",
+        price:"₹ 110"  
+    },{
+        imgUrl:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/395/original/data?1625917064",
+        name:"Baby Orange",
+        qty:"1 kg",
+        price:"₹ 90"  
+    },{
+        imgUrl:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/261/original/data?1612541635",
+        name:"Pomegranate (Anar)",
+        qty:"1 kg",
+        price:"₹ 80" 
+    }
+]
+
+ localStorage.setItem("fruitData",JSON.stringify(fruitData));
