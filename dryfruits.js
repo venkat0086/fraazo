757b78704df16c038e303489f97b14dcb680848f
Ketan_Mangidkar <ketanmangidkar@gmail.com>
2021-12-17 15:09:30 +0530

    dryruits update
    dryfruits updated

---
+++ dryfruits.js
@@ -0,0 +1,94 @@
+var veggie = [{
+        name: "Cashew - 100gm",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/577/original/data?1635085485",
+        price: "99",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Cashew Tukda - 100gm",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/580/original/data?1635086672",
+        price: "92",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Walnut - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/583/original/data?1635137039",
+        price: "175",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Pista Salted - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/586/original/data?1635141850",
+        price: "122",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Pista Kernel - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/588/original/data?1635142184",
+        price: "186",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Almond - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/590/original/data?1635143515",
+        price: "109",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Kismis - 200 gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/593/original/data?1635144647",
+        price: "68",
+        weight: "200 gms"
+    },
+
+
+    {
+        name: "Dry Dates - 400 gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/597/original/data?1635146300",
+        price: "135",
+        weight: "400 gms"
+    }
+]
+
+veggie.map(function(ele) {
+    var div = document.createElement('div');
+
+    var img = document.createElement('img');
+    img.setAttribute('src', ele.src);
+
+    var nameBox = document.createElement('div');
+    nameBox.setAttribute('class', 'namebox');
+    var name = document.createElement('span');
+    name.setAttribute('class', 'name')
+    name.innerText = ele.name;
+    nameBox.append(name);
+
+    var botBox = document.createElement('div');
+    botBox.setAttribute('class', 'botbox')
+    var priceBox = document.createElement('div');
+    var weight = document.createElement('span');
+    weight.setAttribute('class', 'weight')
+    weight.innerText = ele.weight;
+    var price = document.createElement('span');
+    price.setAttribute('class', 'price')
+    price.innerText = '₹ ' + ele.price;
+    var br = document.createElement('br');
+    priceBox.append(weight, br, price);
+    var btn = document.createElement('div');
+    btn.setAttribute('class', 'btn');
+    btn.innerHTML = '<i class="fas fa-cart-plus"></i>&nbsp ADD';
+    botBox.append(priceBox, btn)
+
+    var display = document.querySelector('#display');
+    div.append(img, nameBox, botBox);
+    display.append(div);
+
+    console.log(ele);
+})
+console.log("Connected");
