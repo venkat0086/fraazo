e30491dd39903dcb6e8f5dee8ae8bca50408b7f8
Venkat  R <venkygowdru99@gmail.com>
2021-12-17 22:41:59 +0530

    Update goto.js

--- goto.js
+++ goto.js
@@ -5,11 +5,21 @@ function gotoreg() {
   window.location.href = "signup.html";
 }
 function gotocart() {
-  window.location.href = "";
+  window.location.href = "frz-cart.html";
 }
 function gotologin() {
   window.location.href = "signin.html";
 }
 function gotoindex() {
   window.location.href = "index.html";
+}
+
+function gotoAdd() {
+  window.location.href = "frz-adrs.html";
+}
+function gotoPay() {
+  window.location.href = "";
+}
+function gotoMap() {
+  window.open("https://maps.google.com", "_blank");
 }
